40f379d9b12124b5bb91bc5aa3f86b2336a8b548
ivandanny <ivandanny@IvanDannyMacbookPro.local>
2017-05-31 16:04:52 +0800

    Super Code Upgrade

--- Assets/Scripts/HexGrid.cs
+++ Assets/Scripts/HexGrid.cs
@@ -10,7 +10,7 @@ public class HexGrid : MonoBehaviour {
 	public HexMesh hexMesh;
 
 	//Grid Function Variable
-	public static int gridColumn = 15;
+	public static int gridColumn = 12;
 	public static int gridRow = 15;
 	private int initLand = 0;
 	int[] landCount = new int[gridRow*gridColumn/3];
@@ -38,6 +38,19 @@ public class HexGrid : MonoBehaviour {
 		}
 	}
 	public List<Islands> islands = new List <Islands> () ;
+
+	public class SelectedIslands {
+		public int XPos;
+		public int YPos;
+		public int No;
+
+		public SelectedIslands (int xPos, int yPos, int no) {
+			XPos = xPos;
+			YPos = yPos;
+			No = no;
+		}
+	}
+	public List<SelectedIslands> selectedIslands = new List <SelectedIslands> () ;
 
 	int landCounter = 0;
 	int[,] landList = new int[gridRow, gridColumn];
@@ -81,8 +94,10 @@ public class HexGrid : MonoBehaviour {
 	}
 	
 	//to declare an Island
-	void islandTag (int row, int column, int counter) {
-
+	void islandTag (int row, int column, int counter, int selected) {
+		if (selected == 1) {
+			selectedIslands.Add (new SelectedIslands(row, column,1));
+		}
 		initLand = Mathf.RoundToInt (gridRow * gridColumn * initStart);
 		landList [row, column] = counter;
 		landCount [counter-1]++;
@@ -92,32 +107,32 @@ public class HexGrid : MonoBehaviour {
 				switch (i) {
 				case 0:
 					if (landList[row+1,column] != counter && gridList[row+1,column] == 1 ) {
-						islandTag(row+1,column,counter);
+						islandTag(row+1,column,counter,selected);
 					}
 					break;
 				case 1:
 					if (landList[row,column+1] != counter && gridList[row,column+1] == 1 ) {
-						islandTag(row,column+1,counter);
+						islandTag(row,column+1,counter,selected);
 					}
 					break;
 				case 2:
 					if (landList[row-1,column] != counter && gridList[row-1,column] == 1 ) {
-						islandTag(row-1,column,counter);
+						islandTag(row-1,column,counter,selected);
 					}
 					break;
 				case 3:
 					if (landList[row-1,column-1] != counter && gridList[row-1,column-1] == 1 ) {
-						islandTag(row-1,column-1,counter);
+						islandTag(row-1,column-1,counter,selected);
 					}
 					break;
 				case 4:
 					if (landList[row,column-1] != counter && gridList[row,column-1] == 1 ) {
-						islandTag(row,column-1,counter);
+						islandTag(row,column-1,counter,selected);
 					}
 					break;
 				case 5:
 					if (landList[row+1,column-1] != counter && gridList[row+1,column-1] == 1 ) {
-						islandTag(row+1,column-1,counter);
+						islandTag(row+1,column-1,counter,selected);
 					}
 					break;
 				}
@@ -126,32 +141,32 @@ public class HexGrid : MonoBehaviour {
 				switch (i) {
 				case 0:
 					if (landList[row+1,column+1] != counter && gridList[row+1,column+1] == 1 ) {
-						islandTag(row+1,column+1,counter);
+						islandTag(row+1,column+1,counter,selected);
 					}
 					break;
 				case 1:
 					if (landList[row,column+1] != counter && gridList[row,column+1] == 1 ) {
-						islandTag(row,column+1,counter);
+						islandTag(row,column+1,counter,selected);
 					}
 					break;
 				case 2:
 					if (landList[row-1,column+1] != counter && gridList[row-1,column+1] == 1 ) {
-						islandTag(row-1,column,counter);
+						islandTag(row-1,column,counter,selected);
 					}
 					break;
 				case 3:
 					if (landList[row-1,column] != counter && gridList[row-1,column] == 1 ) {
-						islandTag(row-1,column,counter);
+						islandTag(row-1,column,counter,selected);
 					}
 					break;
 				case 4:
 					if (landList[row,column-1] != counter && gridList[row,column-1] == 1 ) {
-						islandTag(row,column-1,counter);
+						islandTag(row,column-1,counter,selected);
 					}
 					break;
 				case 5:
 					if (landList[row+1,column] != counter && gridList[row+1,column] == 1 ) {
-						islandTag(row+1,column,counter);
+						islandTag(row+1,column,counter,selected);
 					}
 					break;
 				}
@@ -175,10 +190,91 @@ public class HexGrid : MonoBehaviour {
 			if (noise<(1-initStart)) gridList[(x-offset),(y-offset)] = 0;
 			else {
 				gridList[x-offset, y-offset] = 1; 
-					islands.Add (new Islands(x-offset,y-offset,0));
+				islands.Add (new Islands(x-offset,y-offset,0));
 			}
 		}
 	}
+
+	int[] findMax ( int [] corner) {
+		
+		int minX = selectedIslands[0].XPos;
+		int maxX = selectedIslands[0].XPos;
+		int minY = selectedIslands[0].YPos;
+		int maxY = selectedIslands[0].YPos;
+		for(int cnt = 0; cnt < selectedIslands.Count; cnt++) {
+			if (selectedIslands [cnt].XPos < minX)
+				minX = selectedIslands [cnt].XPos;
+			if (selectedIslands [cnt].XPos > maxX)
+				maxX = selectedIslands [cnt].XPos;
+			if (selectedIslands [cnt].YPos < minY)
+				minY = selectedIslands [cnt].YPos;
+			if (selectedIslands [cnt].YPos > maxY)
+				maxY = selectedIslands [cnt].YPos;
+		}
+
+		corner [0] = minX;
+		corner [1] = maxX;
+		corner [2] = minY;
+		corner [3] = maxY;
+
+		return (corner);
+	}
+
+	//Move the first island to connect with the other island
+	void combineIslands () {
+		bool canMove = false;
+		int[] border = { 0, 0, 0, 0 };
+		int x = selectedIslands [0].XPos;
+		int y = selectedIslands [0].YPos;
+		border = findMax (border);
+
+		while (border [1] < (gridRow-1)) {
+			//Go Right
+			while (border [3] < (gridColumn -2)) {
+				canMove = true;
+				for (int cnt = 0; cnt < selectedIslands.Count; cnt++) {
+					selectedIslands [cnt].YPos += 1;
+					x = selectedIslands [cnt].XPos;
+					y = selectedIslands [cnt].YPos;
+					if (y + 2 < gridColumn) {
+						if (gridList [x, y + 1] == 1 && landList [x, y + 1] != 1) {
+							islandTag (x, y + 1, 1, 1);
+							border = findMax (border);
+							canMove = false;
+						}
+					}
+				}
+				if (canMove == true) {
+					border [2]++;
+					border [3]++;
+				}
+			}
+			//Go Left
+			while (border [2] > 0) {
+				canMove = true;
+				for(int cnt = 0 ; cnt < selectedIslands.Count; cnt++) {
+					selectedIslands [cnt].YPos -= 1;
+					x = selectedIslands [cnt].XPos;
+					y = selectedIslands [cnt].YPos;
+					if (y - 1 != 0) {
+						if (gridList [x, y - 1] == 1 && landList [x, y - 1] != 1) {
+							islandTag (x, y - 1, 1, 1);
+							canMove = false;
+							border = findMax (border);
+						}
+					}
+				}
+				if (canMove == true) {
+					border [2]--;
+					border [3]--;
+				}
+			}
+			//Go Up
+			border[1] = gridRow;
+		}
+	}
+
+
 
 	void Awake () {
 		cells = new HexCell[gridRow * gridColumn];
@@ -192,22 +288,31 @@ public class HexGrid : MonoBehaviour {
 		for (int cnt = 0; cnt < islands.Count; cnt++) {
 			if (landList[islands[cnt].XPos,islands[cnt].YPos] == 0) {
 				landCounter++;
-				islandTag(islands[cnt].XPos,islands[cnt].YPos,landCounter);
+				islandTag(islands[cnt].XPos,islands[cnt].YPos,landCounter,0);
 			}	
 			islands [cnt].No = landList [islands [cnt].XPos, islands [cnt].YPos];
+			if (islands [cnt].No == 1) {
+				selectedIslands.Add (new SelectedIslands(islands [cnt].XPos, islands [cnt].YPos,1));
+			}
 		}
 			
+		combineIslands();
+			
 		//Debug Log on Islands
+		/*
 		Debug.Log (landCounter);
 		Debug.Log (islands);
 		for(int cnt = 0; cnt < islands.Count; cnt++) {
 			Debug.Log("X: " + islands[cnt].XPos + "  Y:" + islands[cnt].YPos + "  No:" + islands[cnt].No);
 		}
-			
+		//for(int cnt = 0; cnt < selectedIslands.Count; cnt++) {
+		//	Debug.Log("X: " + selectedIslands[cnt].XPos + "  Y:" + selectedIslands[cnt].YPos + "  No:" + selectedIslands[cnt].No);
+		//}
+		*/	
 
 		//Creating the Cells
-		for(int cnt = 0; cnt < islands.Count; cnt++) {
-			CreateCell(islands[cnt].XPos,islands[cnt].YPos,cnt);
+		for(int cnt = 0; cnt < selectedIslands.Count; cnt++) {
+			CreateCell(selectedIslands[cnt].YPos,selectedIslands[cnt].XPos,cnt);
 		}
 	}
 
