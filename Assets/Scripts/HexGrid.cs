c7e2f4754dc3b177bd364b78fd7f3b429c34be35
ivandanny <ivandanny@IvanDannyMacbookPro.local>
2017-06-19 12:36:47 +0800

    Update on Countries

--- Assets/Scripts/HexGrid.cs
+++ Assets/Scripts/HexGrid.cs
@@ -10,14 +10,24 @@ public class HexGrid : MonoBehaviour {
 	public HexMesh hexMesh;
 
 	//Grid Function Variable
-	public static int gridColumn = 360;
-	public static int gridRow = 360;
+	public static int gridColumn = 150;
+	public static int gridRow = 150;
 
 	//Perlin Funcion Variable
 	public int[,] gridList = new int[gridRow,gridColumn];
+	public float[,] heightList = new float[gridRow, gridColumn];
 	public float initStart = 0.55f;
 	public float zoom = 0.3f;
 	public float shiftIndicator = 0.3f;
+
+	//Variable for Creating Countries
+	public int minIslands = 5;
+	public int maxIslands = 10;
+
+	//Variable for Countries Label
+	public Text cellLabelPrefab;
+	Canvas gridCanvas;
+
 
 	//Variable for Checking Island
 	public class Islands {
@@ -48,12 +58,6 @@ public class HexGrid : MonoBehaviour {
 
 	int landCounter = 0;
 	int[,] landList = new int[gridRow, gridColumn];
-
-	//Label Text on Hexagon
-	public Text cellLabelPrefab;
-	Canvas gridCanvas;
-
-	bool quit = false;
 
 	//Check to prevent array out of Index
 	int[] indexCheck (int x, int y) {
@@ -89,6 +93,8 @@ public class HexGrid : MonoBehaviour {
 	
 	//to declare an Island
 	void islandTag (int row, int column, int counter, int selected) {
+
+		//Is to add the current land to the SelectedIslands group.
 		if (selected == 1) {
 			selectedIslands.Add (new SelectedIslands(row, column,1));
 		}
@@ -174,43 +180,37 @@ public class HexGrid : MonoBehaviour {
 		int offset = Random.Range (0, 10000);
 		for(int x = offset; x < (row+offset); x++)
 			for(int y = offset; y < (column+offset); y++)
-		{
-			Vector2 pos = zoom * (new Vector2(x,y)) + shift;
-			Vector2 pos2 = pos*4;
-			Vector2 pos3 = pos / 3;
-			float noise = Mathf.PerlinNoise(pos.x, pos.y);
-			noise += (Mathf.PerlinNoise ((pos2.x), (pos2.y))-0.5f)/2.5f;
-			noise += (Mathf.PerlinNoise ((pos3.x), (pos3.y)))/1.5f;
-			if (noise<(1.8-initStart)) gridList[(x-offset),(y-offset)] = 0;
-			else {
-				gridList[x-offset, y-offset] = 1; 
-				islands.Add (new Islands(x-offset,y-offset,0));
+			{
+				Vector2 pos = zoom * (new Vector2(x,y)) + shift;
+				Vector2 pos2 = pos*4;
+				Vector2 pos3 = pos / 3;
+				float noise = Mathf.PerlinNoise(pos.x, pos.y);
+				noise += (Mathf.PerlinNoise ((pos2.x), (pos2.y))-0.5f)/2.5f;
+				heightList [x - offset, y - offset] = noise;
+				noise += (Mathf.PerlinNoise ((pos3.x), (pos3.y)))/1.5f;
+				if (noise<(1.8-initStart)) gridList[(x-offset),(y-offset)] = 0;
+				else {
+					gridList[x-offset, y-offset] = 1;
+					islands.Add (new Islands(x-offset,y-offset,0));
+				}
 			}
-		}
 	}
 
 	int[] findMax ( int [] corner) {
-		
-		int minX = selectedIslands[0].XPos;
-		int maxX = selectedIslands[0].XPos;
-		int minY = selectedIslands[0].YPos;
-		int maxY = selectedIslands[0].YPos;
+		corner [0]  = selectedIslands[0].XPos;
+		corner [1]  = selectedIslands[0].XPos;
+		corner [2]  = selectedIslands[0].YPos;
+		corner [3]  = selectedIslands[0].YPos;
 		for(int cnt = 0; cnt < selectedIslands.Count; cnt++) {
-			if (selectedIslands [cnt].XPos < minX)
-				minX = selectedIslands [cnt].XPos;
-			if (selectedIslands [cnt].XPos > maxX)
-				maxX = selectedIslands [cnt].XPos;
-			if (selectedIslands [cnt].YPos < minY)
-				minY = selectedIslands [cnt].YPos;
-			if (selectedIslands [cnt].YPos > maxY)
-				maxY = selectedIslands [cnt].YPos;
+			if (selectedIslands [cnt].XPos < corner [0] )
+				corner [0]  = selectedIslands [cnt].XPos;
+			if (selectedIslands [cnt].XPos > corner [1] )
+				corner [1]  = selectedIslands [cnt].XPos;
+			if (selectedIslands [cnt].YPos < corner [2] )
+				corner [2]  = selectedIslands [cnt].YPos;
+			if (selectedIslands [cnt].YPos > corner [3] )
+				corner [3]  = selectedIslands [cnt].YPos;
 		}
-
-		corner [0] = minX;
-		corner [1] = maxX;
-		corner [2] = minY;
-		corner [3] = maxY;
-		Debug.Log ("FindMax: " + corner [0] + ", " + corner [1] + ", " + corner [2] + ", " + corner [3]);
 		return (corner);
 	}
 
@@ -231,14 +231,12 @@ public class HexGrid : MonoBehaviour {
 			canMove = true;
 			//Go Right
 			while (border [3] < (gridColumn - 2)) {
-				Debug.Log ("Right + 1");
 				for (int cnt = 0; cnt < selectedIslands.Count; cnt++) {
 					selectedIslands [cnt].YPos += 1;
 					x = selectedIslands [cnt].XPos;
 					y = selectedIslands [cnt].YPos;
 					if (y + 1 < gridColumn) {
 						if (gridList [x, y] == 1 && landList [x, y] != 1) {
-							Debug.Log ("Found Right" + landList [x, y]);
 							islandTag (x, y, 1, 1);
 						}
 					}
@@ -254,14 +252,12 @@ public class HexGrid : MonoBehaviour {
 			}
 			//Go Left
 			while (border [2] > 0) {
-				Debug.Log ("Left + 1");
 				for (int cnt = 0; cnt < selectedIslands.Count; cnt++) {
 					selectedIslands [cnt].YPos -= 1;
 					x = selectedIslands [cnt].XPos;
 					y = selectedIslands [cnt].YPos;
 					if (y > 0) {
 						if (gridList [x, y] == 1 && landList [x, y] != 1) {
-							Debug.Log ("Found Left" + landList [x, y]);
 							islandTag (x, y, 1, 1);
 						}
 					}
@@ -280,17 +276,13 @@ public class HexGrid : MonoBehaviour {
 			int first = selectedIslands.Count;
 			int second = 0;
 
-			if (border [1] < gridRow - 2) {
-				Debug.Log ("UP + 1");
+			if (border [1] < gridRow - 3) {
 				for (int cnt = 0; cnt < selectedIslands.Count; cnt++) {
 					selectedIslands [cnt].XPos += 2;
 					x = selectedIslands [cnt].XPos;
 					y = selectedIslands [cnt].YPos;
-					Debug.Log ("UP: " + border [0] + " " + border [1] + " " + border [2] + " " + border [3]);
-					if (x + 2 < gridRow) {
-						Debug.Log ("UP2: " + x + " " + y);
+					if (x < gridRow) {
 						if (gridList [x, y] == 1 && landList [x, y] != 1) {
-							//Debug.Log ("Found Up" + landList [x, y]);
 							islandTag (x, y, 1, 1);
 							second = selectedIslands.Count;
 							for (int i = second-1; i >= first; i-- ) {
@@ -317,10 +309,29 @@ public class HexGrid : MonoBehaviour {
 	}
 
 
+void createCountries () {
+		for(int cnt = 0; cnt < selectedIslands.Count; cnt++) {
+			gridList [selectedIslands [cnt].XPos, selectedIslands [cnt].YPos] = 1;
+		}
+		int count = 1;
+		for(int cnt = 0; cnt < selectedIslands.Count; cnt++) {
+			int size = 0;
+			int x = selectedIslands [cnt].XPos;
+			int y = selectedIslands [cnt].YPos;
+			if (landList [x, y] == 0) {
+				landList [x, y] = count;
+				count += 1;
+			}
+		}
+
+	}
+
+
 
 	void Awake () {
-		cells = new HexCell[gridRow * gridColumn];
+
 		gridCanvas = GetComponentInChildren<Canvas>();
+		cells = new HexCell[gridRow * gridColumn];
 
 		//Creating the Island
 		initRandom(gridRow,gridColumn);
@@ -337,10 +348,17 @@ public class HexGrid : MonoBehaviour {
 				selectedIslands.Add (new SelectedIslands(islands [cnt].XPos, islands [cnt].YPos,1));
 			}
 		}
-			
+
+		//Combine the separated islands into one
 		combineIslands();
 
-		//Creating the Cells
+		//Create Countries
+		landList= new int[gridRow,gridColumn];
+		gridList= new int[gridRow, gridColumn];
+		createCountries();
+
+
+//Creating the Cells
 		for(int cnt = 0; cnt < selectedIslands.Count; cnt++) {
 			CreateCell(selectedIslands[cnt].YPos,selectedIslands[cnt].XPos,cnt);
 		}
@@ -350,7 +368,7 @@ public class HexGrid : MonoBehaviour {
 	void CreateCell (int x, int z, int i) {
 		Vector3 position;
 		position.x = (x + z * 0.5f - z / 2) * (HexMetrics.innerRadius * 2f);
-		position.y = 0f;
+		position.y = heightList[x,z]* 20;
 		position.z = z * (HexMetrics.outerRadius * 1.5f);
 		
 		HexCell cell = cells[i] = Instantiate<HexCell>(cellPrefab);
@@ -361,12 +379,12 @@ public class HexGrid : MonoBehaviour {
 		hex.transform.SetParent(transform, false);
 		hex.transform.localPosition = position;
 
-		/*
+
 		Text label = Instantiate<Text>(cellLabelPrefab);
 		label.rectTransform.SetParent(gridCanvas.transform, false);
-		label.rectTransform.anchoredPosition = new Vector2(position.x, position.z);
-		label.text = z.ToString() + "\n" + x.ToString() + "\n" + landList[z,x].ToString();
-		*/
+		label.rectTransform.anchoredPosition3D = new Vector3(position.x, position.z, (position.y *-1));
+		label.text = landList[z,x].ToString();
+
 	}
 
 	
--- Assets/Scripts/HexMetrics.cs
+++ Assets/Scripts/HexMetrics.cs
@@ -6,13 +6,16 @@ public static class HexMetrics {
 	
 	public const float innerRadius = outerRadius * 0.866025404f;
 
+	public const float smallRadius = outerRadius * 0.95f;
+	public const float smallerRadius = innerRadius * 0.95f;
+
 	public static Vector3[] corners = {
-		new Vector3(0f, 0f, outerRadius),
-		new Vector3(innerRadius, 0f, 0.5f * outerRadius),
-		new Vector3(innerRadius, 0f, -0.5f * outerRadius),
-		new Vector3(0f, 0f, -outerRadius),
-		new Vector3(-innerRadius, 0f, -0.5f * outerRadius),
-		new Vector3(-innerRadius, 0f, 0.5f * outerRadius),
-		new Vector3(0f, 0f, outerRadius)
+		new Vector3(0f, 0f, smallRadius),
+		new Vector3(smallerRadius, 0f, 0.5f * smallRadius),
+		new Vector3(smallerRadius, 0f, -0.5f * smallRadius),
+		new Vector3(0f, 0f, -smallRadius),
+		new Vector3(-smallerRadius, 0f, -0.5f * smallRadius),
+		new Vector3(-smallerRadius, 0f, 0.5f * smallRadius),
+		new Vector3(0f, 0f, smallRadius)
 	};
 }
