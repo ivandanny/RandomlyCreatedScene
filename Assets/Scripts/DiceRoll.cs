8c00676f455c2bf6c5b75a97c0950da8dcf9eef4
ivandanny <danny.ivan@yahoo.com>
2017-08-14 18:02:32 +0800

    Dice number is rolling

---
+++ Assets/Scripts/DiceRoll.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DiceRoll : MonoBehaviour {
+	Text diceValue;
+
+	// Use this for initialization
+	void Start () {
+		diceValue = GetComponent<Text> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		diceValue.text = Random.Range (1, 7).ToString();
+	}
+}
